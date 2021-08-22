7ae4ae2e21c0f3d69a15117dea59cafd7319f196
deeptha <deepthasanushka92@gmail.com>
2021-08-22 23:37:29 +0530

    show curent location in the map

--- app/src/main/java/com/cbl/treeapp/MapsActivity.java
+++ app/src/main/java/com/cbl/treeapp/MapsActivity.java
@@ -1,17 +1,29 @@
 package com.cbl.treeapp;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cbl.treeapp.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,22 +32,35 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import static com.cbl.treeapp.AddNewTreeActivity.isLocationEnabled;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-    private FloatingActionButton floatingActionButtonAdd;
+    private FloatingActionButton floatingActionButtonAdd, floatingActionButtonRefresh;
+    public LocationManager locationManager;
+    public double latitude;
+    public double longitude;
+    public Criteria criteria;
+    public String bestProvider;
+
     ArrayList<LatLng> arrayListTree = new ArrayList<LatLng>();
     LatLng sydney = new LatLng(-34, 151);
     LatLng marker2 = new LatLng(-31.08322, 150.91672);
@@ -56,6 +81,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
 
         floatingActionButtonAdd = findViewById(R.id.FbAddNew);
+        floatingActionButtonRefresh = findViewById(R.id.FbRefresh);
 
         mapFragment.getMapAsync(this);
         arrayListTree.add(sydney);
@@ -69,6 +95,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onClick(View v) {
                 Intent intent = new Intent(MapsActivity.this, AddNewTreeActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        floatingActionButtonRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMapData();
             }
         });
     }
@@ -85,19 +118,138 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        checkPermission();
 
-        // Add a marker in Sydney and move the camera
+        getMapData();
+//
+
+    }
+
+    public void getMapData() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        // Check if we were successful in obtaining the map.
+        if (mMap != null) {
+            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(Location arg0) {
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(arg0.getLatitude(), arg0.getLongitude())).title("It's Me!"));
+//                    CameraPosition old = mMap.getCameraPosition();
+                    CameraPosition pos = new CameraPosition.Builder()
+                            .target(new LatLng(arg0.getLatitude(), arg0.getLongitude()))
+                            .zoom(10)
+                            .build();
+
+                    //mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(pos));
+                }
+            });
+        }
+
+//         Add a marker in Sydney and move the camera
         for (LatLng arrayListTree : arrayListTree
         ) {
             mMap.addMarker(new MarkerOptions().position(arrayListTree).
                     icon(BitmapDescriptorFactory.fromBitmap(
-                            createCustomMarker(MapsActivity.this,R.drawable.tree,"TREE")))).setTitle("Tree Location");
+                            createCustomMarker(MapsActivity.this, R.drawable.tree, "TREE")))).setTitle("Tree Location");
 
             mMap.moveCamera(CameraUpdateFactory.newLatLng(arrayListTree));
         }
 
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                getLocation();
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+            getLocation();
+
+        } else {
+            checkPermission();
+        }
+    }
+
+    protected void getLocation() {
+//        if (isLocationEnabled(AddNewTreeActivity.this)) {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        try {
+            gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        try {
+            network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        if (!gps_enabled && !network_enabled) {
+            // notify user
+            new AlertDialog.Builder(this)
+                    .setMessage("ALERT")
+                    .setPositiveButton("OPEN SETTIGNS", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                            MapsActivity.this.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("CANCEL", null)
+                    .show();
+        } else {
+            if (isLocationEnabled(MapsActivity.this)) {
+                criteria = new Criteria();
+                bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+
+                //You can still do this if you like, you might get lucky:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                Location location = getLastKnownLocation();
+                if (location != null) {
+                    locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+                } else {
+                    //This is what you need:
+                    locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+                }
+            } else {
+                //prompt user to enable location....
+                //.................
+            }
+        }
+
 
     }
+
 
     private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
         Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
@@ -114,7 +266,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         CircleImageView markerImage = (CircleImageView) marker.findViewById(R.id.user_dp);
         markerImage.setImageResource(resource);
-        TextView txt_name = (TextView)marker.findViewById(R.id.name);
+        TextView txt_name = (TextView) marker.findViewById(R.id.name);
         txt_name.setText(_name);
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -129,4 +281,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         return bitmap;
     }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+//
+
+        //open the map:
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+//        Toast.makeText(MapsActivity.this, "latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_SHORT).show();
+//        searchNearestPlace(voice2text);
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    private Location getLastKnownLocation() {
+        locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+
+            @SuppressLint("MissingPermission") Location l = locationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
+    }
+
 }
